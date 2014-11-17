4237162e3b6277cee22ad538436d4d25f46ff151
jrlandau <jrlandau@verizon.net>
2014-11-17 01:37:36 +0000

    changed index.js to serve index.html

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+var fs = require('fs');  
 var express = require('express')
 var app = express();
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+app.get ('/', function  (request, response) {
+  var fileBuffer = fs.readFileSync('index.html');
+  response.send(fileBuffer.toString('utf-8'));
 })
 
 app.listen(app.get('port'), function() {
