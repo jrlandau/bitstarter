68a3085dddb3503b9c97a93e03594eb199a5f8b9
jrlandau <jrlandau@verizon.net>
2014-11-20 00:36:51 +0000

    fix bugs in index.html as in HW6

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var fs = require('fs');  
 var express = require('express')
-var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+var app = express.createServer(express.logger());
+
+//app.set('port', (process.env.PORT || 8080))
+//pp.use(express.static(__dirname + '/public'))
 
 app.get ('/', function  (request, response) {
   var fileBuffer = fs.readFileSync('index.html');
   response.send(fileBuffer.toString('utf-8'));
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+
+//app.listen(app.get('port'), function() {
+//  console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+
+
