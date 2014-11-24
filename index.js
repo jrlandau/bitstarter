4f0bafa07241f1e8310837f1ae5a2ff9cacc9c74
jrlandau <jrlandau@verizon.net>
2014-11-24 06:04:03 +0000

    replace index.js with earlier version

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+// there are 2 versions of this, they may! correspond to different releases of Express.
+// the one below does not seem to work on Heroku
+
+var fs = require('fs');
+var express = require('express')
+var app = express();
+
+  app.set('port', (process.env.PORT || 5000))
+  app.use(express.static(__dirname + '/public'))
+
+  app.get ('/', function (request, response) {
+  var fileBuffer = fs.readFileSync('index.html');
+  response.send(fileBuffer.toString('utf-8'));
+})
+
+  app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
+
+/*
+
 var fs = require('fs');  
 var express = require('express')
 
@@ -19,5 +40,4 @@ app.listen(port, function() {
 //  console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-
-
+*/
