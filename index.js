61aa8a99eb523b13e65f61129fdefef2077e7c59
jrlandau <jrlandau@verizon.net>
2014-11-26 07:00:11 +0000

    remove cool ascii faces

--- index.js
+++ index.js
@@ -3,16 +3,16 @@
 
 var fs = require('fs');
 var express = require('express')
-var cool = require('cool-ascii-faces');
+//var cool = require('cool-ascii-faces');
 var app = express();
 
   app.set('port', (process.env.PORT || 5000))
   app.use(express.static(__dirname + '/public'))
 
   app.get ('/', function (request, response) {
-  response.send(cool());
-//  var fileBuffer = fs.readFileSync('index.html');
-//  response.send(fileBuffer.toString('utf-8'));
+//  response.send(cool());
+  var fileBuffer = fs.readFileSync('index.html');
+  response.send(fileBuffer.toString('utf-8'));
 })
 
   app.listen(app.get('port'), function() {
